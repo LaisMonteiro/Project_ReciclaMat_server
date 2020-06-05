f046b75063b85fc7161b1d8c70aa4399b49e1201
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-05 18:27:41 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.json({ message: 'It is working' });
+});
+
+module.exports = router;
