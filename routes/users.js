41c550f23bbd8df10263586faf110ebfa3e6f641
cristianekoshino <cristianekoshino@gmail.com>
2020-06-08 15:55:36 +0100

    authentication signIn and SignUp

--- routes/users.js
+++ routes/users.js
@@ -2,29 +2,32 @@
 const { Router } = require('express');
 const router = new Router();
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const uploader = require('./../file-uploader');
+
 
 router.get('/', function (req, res, next) {
   res.json({ message: 'WORKIN! USER API' });
 });
 
-router.post('/create', function (req, res) {
-  const { name, email, passwordHash, avatar } = req.body;
-  User.create({
-    name,
-    email,
-    passwordHash,
-    avatar
-  })
-    .then((response) =>
-      res.status(200).json({
-        response: response,
-        name: name,
-        email: email,
-        passwordHash: passwordHash,
-        avatar: avatar
-      })
-    )
-    .catch((error) => res.status(400).json({ message: error._message }));
+router.post('', uploader.single('avatar'), async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const avatar = req.file.url;
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const alredyExists = await User.find({email});
+    if(alredyExists) {
+      throw new Error('User already exists'); 
+    }
+    
+
+    const user = await User.create({ name, email, passwordHash, avatar });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
