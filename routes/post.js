25a237210c4d25b84198ae1bf4ccdf3720deeb17
karenrogersn <56484176+karenrogersn@users.noreply.github.com>
2020-06-11 15:56:04 +0100

    commit

--- routes/post.js
+++ routes/post.js
@@ -3,10 +3,12 @@
 const { Router } = require('express');
 const router = new Router();
 const Post = require('../models/post');
+const User = require('../models/user');
 const uploader = require('./../file-uploader');
 
 router.get('/', function (req, res, next) {
   const kind = req.query.kind;
+  const userCreator = req.query.userCreator;
 
   Post.find(kind ? { kind } : {})
     .sort({ 'timestamps.updatedAt': -1 })
@@ -16,6 +18,13 @@ router.get('/', function (req, res, next) {
       res.json(posts);
     })
     .catch((error) => next(error));
+
+  //   Post.find({ _id: userCreator })
+  //   .then((response)=> {
+  //     res.json(message: 'POST USER API')
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
 });
 
 router.post('/', uploader.single('image'), (req, res, next) => {
